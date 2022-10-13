f7bcec5c1f3631f758f3f1cd2da12dfc10aab900
bhushanu <bhushanu@shiengroup.com>
2022-10-13 00:45:56 +0000

    Refining replication statement to be compatible with MySQL 8.0 Adding more delay statement for clean startup. Commenting out the dbproxy part for timebeing.

--- build.sh
+++ build.sh
@@ -4,37 +4,37 @@ sudo rm -rf ./master/data/*
 for N in 1 2; do
     sudo rm -rf ./slave/data$N/*
 done
+sleep 5
 docker-compose build
 docker-compose up -d
-
-until docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
-sleep 10
+sleep 30
+until docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "show master status \G;"'
 do
     echo "Waiting for mysql_master database connection..."
-    sleep 4
+    sleep 5
 done
 
-priv_stmt='CREATE USER "mydb_slave_user"@"%" IDENTIFIED BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
+priv_stmt='CREATE USER "mydb_slave_user"@"%" IDENTIFIED WITH mysql_native_password BY "mydb_slave_pwd"; GRANT REPLICATION SLAVE ON *.* TO "mydb_slave_user"@"%"; FLUSH PRIVILEGES;'
 docker exec mysql_master sh -c "export MYSQL_PWD=111; mysql -u root -e '$priv_stmt'"
 
-sleep 39
+# sleep 45
 #replica
 for N in 1 2; do
-    until docker-compose exec mysql_slave$N sh -c 'export MYSQL_PWD=111; mysql -u root -e ";"'
+    until docker-compose exec mysql_slave$N sh -c 'export MYSQL_PWD=111; mysql -u root -e "show replica status \G;"'
     do
         echo "Waiting for mysql_slave$N database connection..."
-        sleep 4
+        sleep 5
     done
 
     MS_STATUS=`docker exec mysql_master sh -c 'export MYSQL_PWD=111; mysql -u root -e "SHOW MASTER STATUS"'`
     CURRENT_LOG=`echo $MS_STATUS | awk '{print $6}'`
     CURRENT_POS=`echo $MS_STATUS | awk '{print $7}'`
 
-    start_slave_stmt="CHANGE MASTER TO MASTER_HOST='mysql_master',MASTER_USER='mydb_slave_user',MASTER_PASSWORD='mydb_slave_pwd',MASTER_LOG_FILE='$CURRENT_LOG',MASTER_LOG_POS=$CURRENT_POS; START SLAVE;"
-    start_slave_cmd='export MYSQL_PWD=111; mysql -u root -e "'
-    start_slave_cmd+="$start_slave_stmt"
-    start_slave_cmd+='"'
-    docker exec mysql_slave$N sh -c "$start_slave_cmd"
-
-    docker exec mysql_slave$N sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW SLAVE STATUS \G'"
+    start_replica_stmt="CHANGE REPLICATION SOURCE to SOURCE_HOST='mysql_master',SOURCE_USER='mydb_slave_user',SOURCE_PASSWORD='mydb_slave_pwd', SOURCE_LOG_FILE='$CURRENT_LOG',SOURCE_LOG_POS=$CURRENT_POS; START REPLICA;"
+    start_replica_cmd='export MYSQL_PWD=111; mysql -u root -e "'
+    start_replica_cmd+="$start_replica_stmt"
+    start_replica_cmd+='"'
+    docker exec mysql_slave$N sh -c "$start_replica_cmd"
+    echo "Replication status on mysql_slave$N "
+    docker exec mysql_slave$N sh -c "export MYSQL_PWD=111; mysql -u root -e 'SHOW REPLICA STATUS \G'"
 done
